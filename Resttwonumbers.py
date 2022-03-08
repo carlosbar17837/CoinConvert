aec876e875c8cf6d2a50a5e1752459645515fa32
Carlos <carlosmembreno@Carloss-MacBook-Air.local>
2022-03-08 15:00:31 -0600

    I had to make this commit cause i do not know why it is showing modified on git satus

--- Resttwonumbers.py
+++ Resttwonumbers.py
@@ -1,9 +1,9 @@
-num1 = input ("Ingrese un Numero")
-num1 = float(num1)
+num1 = input ("Ingrese un Numero")   #We create var num1 and its value will be the input 
+num1 = float(num1)                   # we convert this string input into a float var
 
-num2 = input ("Ingrese el Numero que desea restar")
+num2 = input ("Ingrese el Numero que desea restar")         
 num2 = float(num2)
 
-rest = num1 - num2 
-rest = str(rest)
-print ("El resultado es " + rest)
+rest = num1 - num2            #We create a var that will be saving our result 
+rest = str(rest)              # We convert this float valut to a string value so we will be able to concatenate 
+print ("El resultado es " + rest) # we take our string value to make the answer availble 
