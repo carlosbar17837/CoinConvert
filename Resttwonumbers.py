a6ceab17e9fe081e683de3dd22fd90e797590019
Carlos <carlosmembreno@Carloss-MacBook-Air.local>
2022-03-08 14:52:21 -0600

    I've just create a program to rest two numbers and print the result on the screen

---
+++ Resttwonumbers.py
@@ -0,0 +1,9 @@
+num1 = input ("Ingrese un Numero")
+num1 = float(num1)
+
+num2 = input ("Ingrese el Numero que desea restar")
+num2 = float(num2)
+
+rest = num1 - num2 
+rest = str(rest)
+print ("El resultado es " + rest)
